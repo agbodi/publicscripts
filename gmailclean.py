8dea173f71d81ff75cacbdd27de67fb741d6cbc7
root <root@workstation.example.com>
2016-09-26 09:49:31 -0400

    clean email

---
+++ gmailclean.py
@@ -0,0 +1,10 @@
+#Author Ade Gbodimowo
+#Script marks all emails from unread to read.
+
+import imaplib
+myemail = imaplib.IMAP4_SSL('imap.gmail.com', '993')
+##Replace GMAILUSERNAMEHERE with your gmail username and PASSWORDHERE with your secret password
+myemail.login('GMAILUSERNAMEHERE', 'PASSWORDHERE')
+myemail.select('Inbox')
+typ ,data = myemail.search(None,'UnSeen')
+myemail.store(data[0].replace(' ',','),'+FLAGS','\Deleted')
